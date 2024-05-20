bfbcf6ed176fa99bca938f2ea1c37bc4b7b4d8e1
Milcik1985 <johndoe@example.commilcik@gmail.com>
2024-05-20 17:26:56 +0300

    answer endpoints added

--- src/controllers/answers.js
+++ src/controllers/answers.js
@@ -16,14 +16,50 @@ const POST_AN_ANSWER = async (req, res) => {
 
     const savedAnswer = await newAnswer.save();
 
-    res.status(201).json({
+    return res.status(201).json({
       message: "Your Answer Posted Successfully",
       answer: savedAnswer,
     });
   } catch (err) {
     console.log("Handled error:", err);
-    res.status(500).json({ message: "Error occured" });
+    return res.status(500).json({ message: "Error occured" });
   }
 };
 
-export { POST_AN_ANSWER };
+const GET_ALL_ANSWERS = async (req, res) => {
+  try {
+    console.log("Inside GET_ALL_ANSWERS controller function");
+
+    const questionId = req.params.id;
+
+    console.log("Question id:", questionId);
+
+    const answers = await AnswerModel.find({ question_id: questionId });
+    console.log("Retrieved answers:", answers);
+
+    console.log("Retrieved answers:", answers);
+    return res.json({ answers: answers });
+  } catch (err) {
+    console.log("Handled error:", err);
+    return res.status(500).json({ message: "Error occured" });
+  }
+};
+
+const DELETE_AN_ANSWER = async (req, res) => {
+  try {
+    const answer = await AnswerModel.findByIdAndDelete(req.params.id);
+
+    if (!answer) {
+      return res.status(404).json({ message: "Answer does not exist" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "The Answer Deleted", answer: answer });
+  } catch (err) {
+    console.log("Handled error:", err);
+    return res.status(500).json({ message: "Error occured" });
+  }
+};
+
+export { POST_AN_ANSWER, GET_ALL_ANSWERS, DELETE_AN_ANSWER };
--- src/routes/answers.js
+++ src/routes/answers.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
   POST_AN_ANSWER,
-  //   GET_ALL_ANSWERS,
-  //   DELETE_AN_ANSWER,
+  GET_ALL_ANSWERS,
+  DELETE_AN_ANSWER,
 } from "../controllers/answers.js";
 import authUser from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/question/:id/answers", authUser, POST_AN_ANSWER);
-// router.get("/question/:id/answers", GET_ALL_ANSWERS);
-// router.delete("/answer/:id", authUser, DELETE_AN_ANSWER);
+router.get("/question/:id/answers", GET_ALL_ANSWERS);
+router.delete("/answer/:id", authUser, DELETE_AN_ANSWER);
 
 export default router;
